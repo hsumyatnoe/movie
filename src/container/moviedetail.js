b4c8961fcdd9416b94e9bacec00906f584117b5c
htetaungshine2111 <htshine21@gmail.com>
2018-12-16 17:35:27 +0630

    show movie detail

--- src/container/moviedetail.js
+++ src/container/moviedetail.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 class MovieDetail extends Component {
 
+    
     constructor(props) {
         super(props);
         this.state = {
@@ -12,7 +13,7 @@ class MovieDetail extends Component {
 
     componentDidMount() {
 
-        fetch(`http://www.omdbapi.com?i=tt2273962&y=&plot=short&type=series&r=json&apikey=b275eadc`)
+        fetch(`http://www.omdbapi.com?i=${this.props.match.params.imdbID}&y=&plot=short&type=series&r=json&apikey=b275eadc`)
         .then(res => res.json())
             .then(
                 (result) => {
@@ -38,10 +39,28 @@ class MovieDetail extends Component {
                   
                 <React.Fragment>     
                            
-             
-                <div className="row">     afdsgg                                                                    
+                           <div className="row">
+                      
+                                <div class="col-md-4" >
+                                Detail
+                                            
+                                    <img src={this.state.movie.Poster} alt="..." class="img-thumbnail" />
+                                    <div class="caption center-block">Year: {this.state.movie.Year}</div>
+                                    <div class="caption center-block">Runtime: {this.state.movie.Runtime}</div>
+                                    <div class="caption center-block">Genre: {this.state.movie.Genre}</div>
+                                    <div class="caption center-block">Actors: {this.state.movie.Actors}</div>
+                                    <div class="caption center-block">Language: {this.state.movie.Language}</div>
+                                    <div class="caption center-block">Type: { this.state.movie.Type}</div>
+                                   
+
+                                </div>
+
+                           </div>
+{/*              
+                <div className="row">
+                                                                                         
                         <h1>{this.state.movie.Title}</h1>
-                </div>
+                </div> */}
                    
               
                             
